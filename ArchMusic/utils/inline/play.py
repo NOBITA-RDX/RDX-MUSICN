943735a3d6c7c379b8ddcce64c8f70a990291a7a
NOBITA-RDX <141653116+NOBITA-RDX@users.noreply.github.com>
2023-09-26 22:12:31 +0530

    Update play.py

--- ArchMusic/utils/inline/play.py
+++ ArchMusic/utils/inline/play.py
@@ -42,8 +42,8 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     x, y = str(round(played_sec/total_sec,1)).split(".")
     pos = int(y)
 
-    line = "─"
-    circle = "●"
+    line = "━"
+    circle = "♪"
 
     bar = line*(pos-1)
     bar += circle
