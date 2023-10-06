4bbf380daa598197209ce5ea2aaa66732b750e90
NOBITA-RDX <141653116+NOBITA-RDX@users.noreply.github.com>
2023-10-06 15:28:31 +0530

    Update help.py

--- ArchMusic/utils/inline/help.py
+++ ArchMusic/utils/inline/help.py
@@ -18,12 +18,12 @@ from ArchMusic import app
 def help_pannel(_, START: Union[bool, int] = None):
     first = [
         InlineKeyboardButton(
-            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
+            text="˹cʟᴏsᴇ˼", callback_data=f"close"
         )
     ]
     second = [
         InlineKeyboardButton(
-            text=_["BACK_BUTTON"],
+            text="˹Bᴀᴄᴋ˼",
             callback_data=f"settingsback_helper",
         ),
         InlineKeyboardButton(
@@ -35,28 +35,58 @@ def help_pannel(_, START: Union[bool, int] = None):
         [
             [
                 InlineKeyboardButton(
-                    text=_["H_B_1"],
+                    text="˹Aᴅᴍɪɴ˼",
                     callback_data="help_callback hb1",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_2"],
+                    text="˹Aᴜᴛʜ˼",
                     callback_data="help_callback hb2",
                 ),
-            ],
-            [
                 InlineKeyboardButton(
-                    text=_["H_B_3"],
+                    text="˹Bʟᴀᴄᴋʟɪsᴛ˼",
                     callback_data="help_callback hb3",
                 ),
+            ],
+            [
                 InlineKeyboardButton(
-                    text=_["H_B_4"],
+                    text="˹Bʀᴏᴀᴅᴄᴀsᴛ˼",
                     callback_data="help_callback hb4",
+                ),
+                InlineKeyboardButton(
+                    text="˹G-ʙᴀɴ˼",
+                    callback_data="help_callback hb12",
+                ),
+                InlineKeyboardButton(
+                    text="˹Lʏʀɪᴄs˼",
+                    callback_data="help_callback hb5",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text=_["H_B_6"],
-                    callback_data="help_callback hb5",
+                    text="˹Pɪɴɢ˼",
+                    callback_data="help_callback hb7",
+                ),
+                InlineKeyboardButton(
+                    text="˹Pʟᴀʏ˼",
+                    callback_data="help_callback hb8",
+                ),
+                InlineKeyboardButton(
+                    text="˹Pʟᴀʏʟɪsᴛ˼",
+                    callback_data="help_callback hb6",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="˹Vɪᴅᴇᴏᴄʜᴀᴛs˼",
+                    callback_data="help_callback hb10",
+                ),
+                InlineKeyboardButton(
+                    text="˹Sᴛᴀʀᴛ˼",
+                    callback_data="help_callback hb11",
+                ),
+                InlineKeyboardButton(
+                    text="˹Sᴜᴅᴏ˼",
+                    callback_data="help_callback hb9",
                 ),
             ],
             mark,
@@ -75,7 +105,7 @@ def help_back_markup(_):
                 ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"], callback_data=f"close"
-                ),
+                )
             ]
         ]
     )
@@ -86,8 +116,12 @@ def private_help_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?start=help",
+                text="˹ Hᴇʟᴩ ˼",
+                callback_data="settings_back_helper",
+            ),
+        ],
+    ]
+    return buttons
             ),
         ],
     ]
